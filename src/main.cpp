8639c21e22813a7a10d1424a688b3c2c92b6d237
Ailene Tan <aileneymt@gmail.com>
2026-01-18 15:39:28 -0500

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
-#include <cstdlib>
-#include <string>
 #include <cstring>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <netdb.h>
-#include <thread>
+#include <fcntl.h>
+#include <sys/epoll.h>
 
-void handleClient(int client_fd) {
-  std::cout << "Client connected\n";
+void handleClient(int epoll_fd, int client_fd) {
   char buffer[512] {};
-  while (read(client_fd, buffer, sizeof(buffer)) > 0) {
+
+  if (read(client_fd, buffer, sizeof(buffer)) > 0) {
+    std::cout << "Client connected, reading... \n";
     const char * response {"+PONG\r\n"};
     send(client_fd, response, strlen(response), 0);
   }
-  close(client_fd);
+  else {
+    epoll_ctl(epoll_fd, EPOLL_CTL_DEL, client_fd, NULL);
+    close(client_fd);
+    std::cout << "Client disconnected\n";
+  }
+}
+
+void connectClient(int epoll_fd, int server_fd) {
+  struct sockaddr_in client_addr;
+  int client_addr_len = sizeof(client_addr);
+  int client_fd = accept(server_fd, (sockaddr*) &client_addr, (socklen_t*) &client_addr_len);
+  
+  if (client_fd < 0) {
+    std::cerr << "accept failed\n";
+    return;
+  }
+  
+  int ret = fcntl(client_fd, F_SETFL, O_NONBLOCK);
+  if (ret < 0) {
+    std::cerr << "fcntl failed\n";
+    close(client_fd);
+    return;
+  }
+
+  struct epoll_event client_event;
+  client_event.data.fd = client_fd;
+  client_event.events = EPOLLIN;
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, client_fd, &client_event);
+
+  std::cout << "Established connection with new client\n";
 }
 
 int main(int argc, char **argv) {
@@ -57,18 +85,41 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
-  std::cout << "Logs from your program will appear here!\n";
-  
-  
-  // TCP, i get a file descriptor for the client connection
-  while (true) {
-    struct sockaddr_in client_addr;
-    int client_addr_len = sizeof(client_addr);
-    int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-    std::thread thread(handleClient, client_fd);
-    thread.detach();
+  int epoll_fd = epoll_create(1);
+  if (epoll_fd < 0) {
+    std::cerr << "epoll_create failed\n";
+    return 1;
   }
+
+  struct epoll_event server_event;
+  server_event.data.fd = server_fd;
+  server_event.events = EPOLLIN;
+  fcntl(server_fd, F_SETFL, O_NONBLOCK);
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, server_fd, &server_event);
+
+  while (true) {
+    struct epoll_event events[64] {};
+    int num_ready = epoll_wait(epoll_fd, events, sizeof(events) / sizeof(epoll_event), -1);
+    
+    if (num_ready == -1) {
+      std::cerr << "epoll error.\n";
+      break;
+    } else if (num_ready == 0) {
+      std::cerr << "epoll timed out.\n";
+      break;
+    }
+
+    for (int i{0}; i < num_ready; ++i) {
+      if (events[i].data.fd == server_fd) { // we can accept a new client connection request
+        connectClient(epoll_fd, server_fd);
+      }
+      else { // read() from the client and send() a response
+        handleClient(epoll_fd, events[i].data.fd);
+      }
+    }
+
+  }
+  close(epoll_fd);
   close(server_fd);
   return 0;
 }
